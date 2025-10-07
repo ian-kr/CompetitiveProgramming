b39a95be6569a006e11f5fff039f923cfeffe4d7
Ian Kristona <ikristona@moscow.ptl.ucf.edu>
2025-10-06 20:40:13 -0400

    working on apple division

--- CSES/intro_problems/apple_division.cpp
+++ CSES/intro_problems/apple_division.cpp
@@ -5,10 +5,14 @@ using namespace std;
 int n;
 vector<long long> nums;
 vector<bool> used;
+int mainSum = 0;
 long long res = -1;
+set<long long> sums;
 
 void bt(int idx, int sum1){
     // We can check the difference at every branch
+    if(sums.count(sum1)) return;
+    sums.insert(sum1);
     long long sum2 = 0;
     for(int i = 0; i < n; i++){
         // add the "non used" can be assumed to be contained in sum2
@@ -16,9 +20,9 @@ void bt(int idx, int sum1){
         sum2+=nums[i];
     }
     (res!=-1) ? res = min(res,abs(sum1-sum2)) : res = abs(sum1-sum2);
-
     // No further changes can be made
     if(idx == n) return;
+    //if(mainSum == (sum1 + sum2)) return;
 
     for(int i = 0; i < n; i++){
         if(used[i]) continue;
@@ -36,7 +40,10 @@ int main(void){
     cin >> n;
     nums =  vector<long long>(n,0);
     used = vector<bool>(n,false);
-    for(auto& i: nums) cin >> i;
+    for(auto& i: nums) {
+        cin >> i;
+        mainSum+=i;
+    }
 
     bt(0,0);
 
